9ad2a201d3eb6a53e720fbb633041703b2b39de8
andreygoncalvess <goncalvessandreyy@gmail.com>
2017-05-08 17:05:22 -0300

    Update multi_prod_cons.cpp

--- PJ2/0505/multi_prod_cons.cpp
+++ PJ2/0505/multi_prod_cons.cpp
@@ -1,130 +1,277 @@
 #include <iostream>
+
 #include <fstream>
+
 #include <uuid/uuid.h>
+
 #include <unistd.h>
+
 #include <errno.h>
+
 #include "mutex.h"
+
 #include "semaphore.h"
+
 #include "thread.h"
 
  
+
 using namespace std;
+
  
+
 const int PRODS = 1;
+
 const int REP = 10;
+
 const int CONS = 1;
+
 const int BUF_SIZE = 1;
- 
+
+int end=0; 
+
 Thread * prods[PRODS];
+
 Thread * cons[CONS];
+
+Mutex mproducer, mconsumer, mend;
+
+Semaphore vazio[BUF_SIZE];
+
+Semaphore cheio[BUF_SIZE];
+
  
+
 uuid_t buffer[BUF_SIZE];
+
 static int prod_pos = 0;
+
 static int cons_pos = 0;
+
  
+
 bool finished = false;
 
-Mutex mut;
 
-Semaphore sem1(1),sem2(0);
+
  
+
 void* producer(void * arg)
+
 {
-	mut.lock();
+
 	cout << "Producer was born!" << endl;
-	mut.unlock();
-	
-	int rep = REP;
+
+	int trep = REP;
+
+	int prod=0;
+
 	char fname[36+1];
 
 
-	while(rep--)
+
+
+
+	while(trep--)
+
 	{
-		sem1.p();
+
+		mproducer.lock();
+
+		prod=prod_pos;
+
 		if(++prod_pos == BUF_SIZE) prod_pos = 0;
-		uuid_generate(buffer[prod_pos]);
-		uuid_unparse(buffer[prod_pos], fname);
+
+		mproducer.unlock();
+
+		vazio[prod].p();	
+
+		uuid_generate(buffer[prod]);
+
+		cheio[prod].v();
+
+		uuid_unparse(buffer[prod], fname);
+
 		string name(fname,sizeof(uuid_t)*2 + 4);
+
 		ofstream file(name.c_str());
+
 		file << name;
-		sem2.v();
+
 		file.close();
+
 	}
 
+
+
  
+
 	exit(REP);
+
 }
+
  
+
 void* consumer(void* arg)
+
 {
-	mut.lock();
+
 	cout << "Consumer was born!" << endl;
-	mut.unlock(); 
+
+
 
 	char fname[36+1];
-	int consumed = 0;
- 
+
+	int j=0;
+
+	int consumo=0; 
+
 	while(true)
+
 	{
 
-		if(finished) exit(consumed);
- 
-		consumed++;
- 
+
+
+	if(finished) exit(consumo);
+
+		sync();		 
+
+		mconsumer.lock();
+
+		consumo=cons_pos; 
+
 		if(++cons_pos == BUF_SIZE) cons_pos = 0;
-		uuid_unparse(buffer[cons_pos], fname);
- 
+
+		mconsumer.unlock();
+
+
+
+		cheio[consumo].p();
+
+		uuid_unparse(buffer[consumo], fname);
+
+ 		j++;
+
+		vazio[consumo].v();
+
 		{
+
 			ifstream file(fname);
+
 			if(!file.good()) continue;
+
 			string str;
-			sem2.p();
+
 			file >> str;
-			sem1.v();
-			mut.lock();
+
 			cout << "Consumed: " << str << endl;
-			mut.unlock();
+
 		}
- 
+
 		if(remove(fname)) cerr << "Error: " << errno << endl;
+
+		mend.lock();
+
+		end++;
+
+		mend.unlock();
+
+		
+
 	}
 
-	exit(consumed);
+
+
+	exit(consumo);
+
 }
+
  
-int main()
-{
+
+int main(){
+
+
+
+    for(int i=0;i<BUF_SIZE;i++){
+
+	vazio[i].p();
+
+    }
+
+
+
     cout << "Massive Producer x Consumer Problem\n";
+
  
+
     // Create
+
     for(int i = 0; i < PRODS; i++)
-    	prods[i] = new Thread(&producer, 0, 0);
+
+    	prods[i] = new Thread(&producer, 0,0);
+
     for(int i = 0; i < CONS; i++)
-    	cons[i] = new Thread(&consumer, 0, 0);
+
+    	cons[i] = new Thread(&consumer, 0,0);
+
  
+
     // Join
+
     int status = 0;
+
     int produced = 0;
-    int consumed = 0;
+
+    int con = 0;
+
     for(int i = 0; i < PRODS; i++)
+
     {
+
     	prods[i]->join(&status);
+
     	produced += status;
+
     }
- 
+
+   sleep(1);
+
+   if(end == produced){
+
+	for(int i = 0;i < BUF_SIZE;i++){
+
+		cheio[i].v();
+
+		vazio[i].v();
+
+	}
+
+    
+
+   
+
     finished = true;
- 
-    for(int i = 0; i < CONS; i++)
+
+   } 
+
+
+
+	 for(int i = 0; i < CONS; i++)
+
     {
+
     	cons[i]->join(&status);
-    	consumed += status;
+
+    	con += status;
+
     }
+
  
-    mut.lock();
+
     cout << "Total produced: " << produced << endl;
-    mut.unlock();
-    mut.lock();
-    cout << "Total consumed: " << consumed << endl;
-    mut.unlock();
+
+    cout << "Total consumed: " << con << endl;
+
     return 0;
+
 }
